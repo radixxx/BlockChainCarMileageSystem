fe5b3e5cab2110ff90d7be45d8cd3d86213db4cc
radixxx <radov.2011@gmail.com>
2019-03-22 19:10:04 +0200

    returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"

--- src/sample/model/StringUtil.java
+++ src/sample/model/StringUtil.java
@@ -58,4 +58,9 @@ public class StringUtil {
         }
     }
 
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
+
 }
