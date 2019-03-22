1144d7551364727e91baafc0340e16883c62bc28
radixxx <radov.2011@gmail.com>
2019-03-22 18:02:14 +0200

    add two new classes Block & Transaction

---
+++ src/sample/model/Block.java
@@ -0,0 +1,26 @@
+package sample.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Block {
+    public String hash;
+    public String previousHash;
+    public String merkleRoot;
+    public ArrayList<Transaction> transactions = new ArrayList<Transaction>(); //our data will be a simple message.
+    public long timeStamp; //as number of milliseconds since 1/1/1970.
+    public int nonce;
+
+    //Block Constructor.
+    public Block(String previousHash) {
+        this.previousHash = previousHash;
+        this.timeStamp = new Date().getTime();
+
+        this.hash = calculateHash(); //Making sure we do this after we set the other values.
+    }
+
+    public Block() {
+    }
+
+
+}
