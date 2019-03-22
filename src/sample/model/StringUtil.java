e0088db87e1b4e1484171b5a038a04f1a46da6da
radixxx <radov.2011@gmail.com>
2019-03-22 19:11:19 +0200

    add a getMerkleRoot func & getDificultyString

--- src/sample/model/StringUtil.java
+++ src/sample/model/StringUtil.java
@@ -1,9 +1,9 @@
 package sample.model;
 
-import java.security.MessageDigest;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.Signature;
+import java.security.*;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 public class StringUtil {
     //Applies Sha256 to a string and returns the result.
@@ -61,6 +61,32 @@ public class StringUtil {
     //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
     public static String getDificultyString(int difficulty) {
         return new String(new char[difficulty]).replace('\0', '0');
+    }
+
+    public static String getStringFromKey(Key key) {
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+
+        List<String> previousTreeLayer = new ArrayList<String>();
+        for(Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+        List<String> treeLayer = previousTreeLayer;
+
+        while(count > 1) {
+            treeLayer = new ArrayList<String>();
+            for(int i=1; i < previousTreeLayer.size(); i+=2) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
     }
 
 }
