8a0bffbdad4ed16fe9f382cc6c36191a8345f704
radixxx <radov.2011@gmail.com>
2019-03-24 11:13:30 +0200

    add library bcprov ver.161

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,9 +5,20 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.model.TransactionOutput;
+
+import java.util.HashMap;
 
 
 public class Main extends Application {
+
+
+
+    public static HashMap<String, TransactionOutput> UTXOs = new HashMap<>();
+    public static int difficulty = 2;
+    public static float minimumTransaction = 0.1f;
+    public static double mileageInterval1 = 110110f;
+    public static double mileageInterval2 = 5500f;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -16,6 +27,8 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
