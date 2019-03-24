08586c67034b059a6818ac12df76d4be5ca37bf3
radixxx <radov.2011@gmail.com>
2019-03-24 11:15:57 +0200

    add new class Global, for keeping static data

---
+++ src/sample/model/Gloabal.java
@@ -0,0 +1,37 @@
+package sample.model;
+
+import sample.Main;
+
+public class Gloabal {
+
+    public static Main mm;
+    public static Block genesis;
+    public static Block block1;
+    public static Block block2;
+    public static Block block3;
+    public static Block block4;
+    public static Block block5;
+    public static Block block6;
+    public static Block block7;
+    public static Block block8;
+
+   // public static Simulation simulation;
+    public static float allTripValue;
+
+    public static float resourceSendingValue;
+
+    public static Repository ECU;
+    public static Repository Oil;
+    public static Repository Drive;
+    public static Repository Generator;
+    public static Repository Intercoler;
+    public static Repository Headlights;
+    public static Repository Transmission;
+    public static Repository SpeedOmetr;
+
+
+    public static Repository repository;
+
+    public static Transaction genesisTransaction;
+
+}
