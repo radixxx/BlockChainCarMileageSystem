60e923d92846a22e65154e93193e721b041dc148
radixxx <radov.2011@gmail.com>
2019-04-01 22:39:34 +0300

    add class SimulationController V.2

--- src/sample/controller/SimulationController.java
+++ src/sample/controller/SimulationController.java
@@ -10,26 +10,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import sample.Main;
-import sample.model.Block;
+import sample.simulation.Simulation;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import static sample.model.Gloabal.*;
-import static sample.model.Validation.addBlock;
-//import static sample.model.Validation.addBlock;
+import static sample.model.Global.simulation;
 
-public class StatisticsController {
+public class SimulationController implements Initializable {
 
-    private Main mainApp;
 
-    @FXML
-    private AnchorPane statisticsRoot;
     @FXML
     private ResourceBundle resources;
 
@@ -37,7 +31,25 @@ public class StatisticsController {
     private URL location;
 
     @FXML
-    private Button backBtn;
+    private Button btnBackSample;
+
+    @FXML
+    private Button btnSimulation;
+
+    @FXML
+    private Label lblSpeed;
+
+    @FXML
+    private Label lblTrip;
+
+    @FXML
+    private Text textTrip;
+
+    @FXML
+    private Text textMileage;
+
+    @FXML
+    private ImageView imgX;
 
     @FXML
     private ImageView img1;
@@ -49,10 +61,10 @@ public class StatisticsController {
     private ImageView img3;
 
     @FXML
-    private ImageView img4;
+    private ImageView img5;
 
     @FXML
-    private ImageView img5;
+    private ImageView img4;
 
     @FXML
     private ImageView img6;
@@ -61,34 +73,7 @@ public class StatisticsController {
     private ImageView img7;
 
     @FXML
-    private ImageView img8;
-
-    @FXML
-    private Button btnEcu;
-
-    @FXML
-    private Button btnOil;
-
-    @FXML
-    private Button btnDrive;
-
-    @FXML
-    private Button btnGenerator;
-
-    @FXML
-    private Button btnIntercoler;
-
-    @FXML
-    private Button btnHedlights;
-
-    @FXML
-    private Button btnTransmision;
-
-    @FXML
-    private Button btnSpeedometer;
-
-    @FXML
-    private Label lbl4;
+    private HBox progressLbl;
 
     @FXML
     private Label lbl1;
@@ -100,146 +85,48 @@ public class StatisticsController {
     private Label lbl3;
 
     @FXML
+    private Label lbl4;
+
+    @FXML
     private Label lbl5;
 
     @FXML
-    private Text text1;
+    private Label lbl6;
 
     @FXML
-    private Text text2;
+    private Label lbl7;
 
     @FXML
-    private Text text3;
+    private Label lbl8;
 
-    @FXML
-    private Text text4;
-
-    @FXML
-    private ImageView imgBlock;
-
-    @FXML
-    private ImageView imgTrans;
-
-    @FXML
-    private ImageView imgBalance;
-
-    @FXML
-    private ImageView imgMileage;
 
     @FXML
     void initialize() {
-
 
     }
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        btnSimulation.setTooltip(new Tooltip("Press to Simulate"));
 
-        btnEcu.setTooltip(new Tooltip("ECU Balance"));
-        btnOil.setTooltip(new Tooltip("Oil Balance"));
-        btnDrive.setTooltip(new Tooltip("Drive Balance"));
-        btnGenerator.setTooltip(new Tooltip("Generator Balance"));
-        btnIntercoler.setTooltip(new Tooltip("Intercoler Balance"));
-        btnHedlights.setTooltip(new Tooltip("HeadLights Balance"));
-        btnTransmision.setTooltip(new Tooltip("Transmission Balance"));
-        btnSpeedometer.setTooltip(new Tooltip("SpeedOmetr Balance"));
-
-
-        btnEcu.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block1 = new Block(genesis.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to ECU...+" +
-                    block1.addTransaction(ECU.sendMileage(ECU.publicKey, 15730))));
-            addBlock(block1);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf("Oil's balance is: " + ECU.getBalance()));
+        btnSimulation.setOnAction((event) -> {
+            simulation = new Simulation();
+            lblTrip.setText(String.valueOf(simulation.getAllTripValue()));
+            lblSpeed.setText(String.valueOf("Speed on the this trip is: " + simulation.calculateRandomSpeedPivot()) + " km/h");
 
         });
-
-
-        btnOil.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block2 = new Block(block1.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Oil..."
-                    + block1.addTransaction(ECU.sendMileage(Oil.publicKey, 15730))));
-            addBlock(block2);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Oil.getBalance()));
-        });
-
-        btnDrive.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block3 = new Block(block2.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Drive..."
-                    + block1.addTransaction(ECU.sendMileage(Drive.publicKey, 15730))));
-            addBlock(block3);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Drive.getBalance()));
-
-        });
-
-        btnGenerator.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block4 = new Block(block3.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Generator..."
-                    + block1.addTransaction(ECU.sendMileage(Generator.publicKey, 15730))));
-            addBlock(block4);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Generator.getBalance()));
-
-        });
-        btnIntercoler.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block5 = new Block(block4.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Intercoler..."
-                    + block1.addTransaction(ECU.sendMileage(Intercoler.publicKey, 15730))));
-            addBlock(block5);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Intercoler.getBalance()));
-
-        });
-
-        btnHedlights.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block6 = new Block(block5.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Headlights..."
-                    + block1.addTransaction(ECU.sendMileage(Headlights.publicKey, 15730))));
-            addBlock(block6);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Headlights.getBalance()));
-        });
-
-        btnTransmision.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block7 = new Block(block6.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to Transmission..."
-                    + block1.addTransaction(ECU.sendMileage(Transmission.publicKey, 15730))));
-            addBlock(block7);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(Transmission.getBalance()));
-        });
-
-        btnSpeedometer.setOnAction((event) -> {
-            lbl1.setText(String.valueOf(block8 = new Block(block7.hash)));
-            lbl2.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl3.setText(String.valueOf("\nECU is attempting to send  mileage to SpeedOmetr..."
-                    + block1.addTransaction(ECU.sendMileage(SpeedOmetr.publicKey, 15730))));
-            addBlock(block8);
-            lbl4.setText(String.valueOf("\nECU's balance is: " + ECU.getBalance()));
-            lbl5.setText(String.valueOf(SpeedOmetr.getBalance()));
-        });
-
 
     }
 
-    public void changeScreen2ButtonPushed(javafx.event.ActionEvent event) throws IOException {
+    public void changeScreen4ButtonPushed(javafx.event.ActionEvent event) throws IOException {
         Parent tableViewParent = FXMLLoader.load(getClass().getResource("/sample/view/sample.fxml"));
         Scene tableViewScene = new Scene(tableViewParent);
         //This line gets the Stage information
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
         window.setScene(tableViewScene);
         window.show();
+
 
     }
 
