3c7c0fa083cccd88e469df4759eae63fdd7acbb8
radixxx <radov.2011@gmail.com>
2019-04-02 13:05:32 +0300

    add fields in class Simulation

--- src/sample/simulation/Simulation.java
+++ src/sample/simulation/Simulation.java
@@ -1,4 +1,14 @@
 package sample.simulation;
 
 public class Simulation {
+    public double startPath;
+    public double endPath;
+
+    public double a = 2000000; // start interval
+    public double b = 4500000; // end interval
+
+    public double time;
+    public double speed;
+
+
 }
