e4ab531fc2aeedef6607356f5f85786ec4ce261e
radixxx <radov.2011@gmail.com>
2019-04-02 13:08:13 +0300

    add new method calculateRandomSpeedPivot in class Simulation

--- src/sample/simulation/Simulation.java
+++ src/sample/simulation/Simulation.java
@@ -1,5 +1,7 @@
 package sample.simulation;
 
+import static sample.model.Gloabal.allTripValue;
+
 public class Simulation {
     public double startPath;
     public double endPath;
@@ -16,6 +18,17 @@ public class Simulation {
         speed = time / allTrip;
         System.out.println("Speed on the this trip is: " + speed + " km/h");
         return speed;
+    }
+
+    public double calculateRandomSpeedPivot() {
+        startPath = (allTripValue / 2);
+        endPath = startPath;
+        time = a + (Math.random() * b);
+        double speed1 = time / startPath;
+        double speed2 = time / endPath + 10;
+        speed = (speed1 + speed2) / 2;
+        System.out.println("Speed on the this trip is: " + (int) speed + " km/h");
+        return (int) speed;
     }
 
 }
